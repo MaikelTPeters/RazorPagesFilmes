9e0eae8667e311f959dd219e35da46572f54b865
MaikelTPeters <maikelpeters10@gmail.com>
2024-08-15 21:45:16 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/Model/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        
+        [Display (Name = "Release Data")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseData { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ RazorPagesFilmes/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Data;
+using RazorPagesFilmes.Model;
+
+namespace RazorPagesFilmes.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesFilmes.Data.RazorPagesFilmesContext _context;
+
+        public IndexModel(RazorPagesFilmes.Data.RazorPagesFilmesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string GenreMovie { get;set; }
+        public SelectList Genres { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+
+            if(!string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                movies = movies.Where(f => f.Title.Contains(SearchTerm));                
+            }
+
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
